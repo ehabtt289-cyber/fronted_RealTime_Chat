ac3f3dbe2d1f90d600079800db4735266a0d5684
Ehab Naser <ehabtt289@gmail.com>
2026-05-31 17:53:39 +0300

    Update useAuthStore.js

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -2,8 +2,7 @@ import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
-
-const BASE_URL = import.meta.env.MODE === "development" ? "https://fullstack-realtime-chat-5sbu.onrender.com/" : "/";
+const BASE_URL = "https://fullstack-realtime-chat-5sbu.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
