41272d9204fc84d831cb5f2c81baab000a133a4f
Ehab Naser <ehabtt289@gmail.com>
2026-05-31 16:41:04 +0300

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL:  "https://fullstack-realtime-chat-5sbu.onrender.com/api",
   withCredentials: true,
 });
