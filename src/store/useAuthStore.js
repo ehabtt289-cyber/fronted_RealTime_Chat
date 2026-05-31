42c3d43ae41e2924a9bd15b3efe30d1369fe38c3
Ehab Naser <ehabtt289@gmail.com>
2026-05-31 16:35:30 +0300

    Update useAuthStore.js

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -16,7 +16,7 @@ export const useAuthStore = create((set, get) => ({
 
   checkAuth: async () => {
     try {
-      const res = await axiosInstance.get("/auth/check");
+      const res = await axiosInstance.get("https://fullstack-realtime-chat-5sbu.onrender.com/api/auth/signup");
 
       set({ authUser: res.data });
       get().connectSocket();
